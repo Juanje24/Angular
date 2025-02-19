2b64f3e953ab103bdac54cba86667a776c22fcee
Juan Jesús Ortiz García <juanje.ortizgarcia@yahoo.es>
2025-02-19 13:45:49 +0100

    Video 11

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from "./top-header/top-header.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
